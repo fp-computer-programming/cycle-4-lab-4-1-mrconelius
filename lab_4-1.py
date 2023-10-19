b3efc4d5da513739ff38a24122b4caf6563f00f1
mrconelius <144148053+mrconelius@users.noreply.github.com>
2023-10-19 12:22:36 -0400

    Add files via upload

---
+++ lab_4-1.py
@@ -0,0 +1,17 @@
+#Author: Matthew Conelius
+
+#Prompt the user to enter their first name and store it
+first_name = input("Matthew")
+
+#Prompt the user to enter their last name and store it
+last_name = input("Conelius")
+
+#Create a new variable for the user's full name 
+full_name = first_name + " " + last_name
+
+#Print the users full name
+print("Full name:", full_name)
+
+#the code asks for the first and last name 
+#this output doesn't provide any space or sperator betwen the first and last name
+#To fix this you can add spacewhile linking the first and last names 
